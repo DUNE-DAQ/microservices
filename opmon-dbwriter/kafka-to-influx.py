3c39d41fe2f5cc73213d0c0013992a056340996b
Marco Roda <marco.roda.88@gmail.com>
2022-06-27 16:54:11 +0100

    Initial draft

--- opmon-dbwriter/kafka-to-influx.py
+++ opmon-dbwriter/kafka-to-influx.py
@@ -17,8 +17,10 @@ import click
               help='port of the kafka broker')       
 @click.option('--kafka-topics', default=['opmon'],
               help='topics of the kafka broker')
+@click.option('--kafka-consumer-id', type=click.STRING, default='microservice',
+              help='id of the kafka consumer, not really important')
 @click.option('--kafka-consumer-group', type=click.STRING, default='opmon_microservice',
-              help='topics of the kafka broker')
+              help='group ID of the kafka consumer, very important to be unique or information will not be duplicated')
 @click.option('--influxdb-address', type=click.STRING, default='opmondb.cern.ch',
               help='address of the influx db')
 @click.option('--influxdb-port', type=click.INT, default=31002,
@@ -28,91 +30,29 @@ import click
 @click.option('--influxdb-name', type=click.STRING, default='influxdb',
               help='name used in the influxdb query')
 
-def cli(kakfa_broker, karkfa_port, kafka_topics, 
+def cli(kakfa_broker, karkfa_port, kafka_topics, kafka_client_id, kafka_group_id, 
         influx_address, influx_port, influx_path, influx_name):
 
-    consumer = KafkaConsumer('erskafka-reporting',
-                            bootstrap_servers='monkafka.cern.ch:30092',
-                            group_id='group1')
-
-    # Get credentials, it expects a file with the following fields, each in a different line:
-
-    # host
-    # port
-    # user
-    # password
-    # dbname
-
-    with open(filename) as f:
-        host, port, user, password, dbname = f.read().split('\n')
-
-    try:
-        con = psycopg2.connect(host=host,
-                            port=port,
-                            user=user,
-                            password=password,
-                            dbname=dbname)
-    except:
-        print('Connection to the database failed, aborting...')
-        exit()
-
-    # These are the fields in the ERS messages, see erskafka/src/KafkaStream.cpp
-    fields = ["partition", "issue_name", "message", "severity", "usecs_since_epoch", "time",
-            "qualifiers", "params", "cwd", "file_name", "function_name", "host_name",
-            "package_name", "user_name", "application_name", "user_id", "process_id",
-            "thread_id", "line_number", "chain"]
-
-    cur = con.cursor()
-
-    # Uncomment to clean the database
-    # cur.execute('''
-    #             DROP TABLE public."ErrorReports";
-    #             ''')
-    # con.commit()
-    # exit()
-
-    # Uncomment to create the table used for the database
-    # cur.execute('''
-    #             CREATE TABLE public."ErrorReports" (
-    #             partition           TEXT,
-    #             issue_name          TEXT,
-    #             message             TEXT,
-    #             severity            TEXT,
-    #             usecs_since_epoch   BIGINT,
-    #             time                BIGINT,
-    #             qualifiers          TEXT,
-    #             params              TEXT,
-    #             cwd                 TEXT,
-    #             file_name           TEXT,
-    #             function_name       TEXT,
-    #             host_name           TEXT,
-    #             package_name        TEXT,
-    #             user_name           TEXT,
-    #             application_name    TEXT,
-    #             user_id             INT,
-    #             process_id          INT,
-    #             thread_id           INT,
-    #             line_number         INT,
-    #             chain               TEXT
-    #            );
-    #            '''
-    #             )
-    # con.commit()
-    # exit()
+    consumer = KafkaConsumer(kafka_topics,
+                            bootstrap_servers=kakfa_broker,
+                            group_id=kafka_group_id, 
+                            client_id=kafka_client_id)
 
     # Infinite loop over the kafka messages
     for message in consumer:
         js = json.loads(message.value)
+        print js
+
         ls = [str(js[key]) for key in fields]
 
-        try:
-            cur.execute(f'INSERT INTO public."ErrorReports" ({",".join(fields)}) VALUES({("%s, " * len(ls))[:-2]})', ls)
-        except:
-            print('Query to insert in the database failed. This is the message received')
-            print(message)
-
-        # Save the insert (or any change) to the database
-        con.commit()
+#        try:
+#            cur.execute(f'INSERT INTO public."ErrorReports" ({",".join(fields)}) VALUES({("%s, " * len(ls))[:-2]})', ls)
+#        except:
+#            print('Query to insert in the database failed. This is the message received')
+#            print(message)
+#
+#        # Save the insert (or any change) to the database
+#        con.commit()
 
 if __name__ == '__main__':
     cli()
