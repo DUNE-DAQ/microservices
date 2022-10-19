d2561445acd301428ccc952ee4b9b7ae96ce13ee
Gordon Crone <g.crone@ucl.ac.uk>
2022-10-19 16:33:17 +0100

    Update connection service to use simple strings for connection ids and regular expressions for matching

--- connection-service/connection-flask.py
+++ connection-service/connection-flask.py
@@ -7,63 +7,18 @@
 
 import os
 import json
-
+import re
+from datetime import datetime, timedelta
+from collections import namedtuple
 from flask import Flask, request, abort
 
-class configmgr(object):
-  def __init__(self, name):
-    self.name=name
-
-    if name != "":
-      self.store={}
-      if "namespace" in os.environ:
-        self.namespace=os.environ["namespace"]
-      else:
-        self.namespace="default"
-
-        from kubernetes import client, config
-        config.load_config()
-        self.core_api = client.CoreV1Api()
-        try:
-          body=self.core_api.read_namespaced_config_map(name,self.namespace)
-        except Exception as ex:
-          print (f"Creating configMap {name}")
-          body=client.V1ConfigMap(api_version="v1",
-                                  metadata={"name":name},
-                                  data={"app":""}
-                                  )
-          self.core_api.create_namespaced_config_map(self.namespace,body)
-        for item in body.data:
-          if body.data[item]!='{}':
-            self.store[item]=body.data[item]
-      print(self.store)
-
-  def update(self, newdict):
-    if self.name != "":
-      body=self.core_api.read_namespaced_config_map(self.name,self.namespace)
-      for item in newdict:
-        body.data[item]=json.dumps(newdict[item])
-      self.core_api.patch_namespaced_config_map(self.name,self.namespace,body)
-  def delete(self, item):
-    if self.name != "":
-      body=self.core_api.read_namespaced_config_map(self.name,self.namespace)
-      if item in body.data:
-        body.data[item]='{}'
-        #print(f"body.data=<{body.data}>")
-      self.core_api.patch_namespaced_config_map(self.name,self.namespace,body)
 
 partitions={}
-#store={'app':{},'source':{},'sourceconn':{},'connection':{}}
-if "configMap" in os.environ:
-  cmgr=configmgr(os.environ["configMap"])
-  for item in cmgr.store:
-    partitions[item]={}
-    if cmgr.store[item]!="":
-      pdict=json.loads(cmgr.store[item])
-      for a in pdict:
-        partitions[item][a]=pdict[a]
+if 'ENTRY_TTL' in os.environ:
+  ttl=int(os.environ['ENTRY_TTL'])
 else:
-  cmgr=configmgr("")
+  ttl=10
+entry_ttl=timedelta(seconds=ttl)
 app=Flask(__name__)
 
 @app.route("/")
@@ -72,130 +27,88 @@ def dump():
   for p in partitions:
     store=partitions[p]
     d=d+f'<h2>Partition {p}</h2> '
-    for n in store:
-      d=d+f'<h3>{n}</h3> <pre>{store[n]}</pre>'
-  print(d)
+    for k,v in store.items():
+      d=d+f'<pre>{k}:  {v}</pre>'
+  #print(d)
   return d
 
 @app.route("/publish",methods=['POST'])
 def publish():
-  #  Store uri associated with an endpoint. If a connection is given,
-  # use the bind_endpoint and uri of the connection otherwise expect
-  # the endpint and uri to be given as part of the form
+  #  Store uri associated with a connection id in the dictionary of
+  #  the appropriate partition
 
   #print(f"publish() request=[{request.form}]")
   part=request.form['partition']
   if part in partitions:
     store=partitions[part]
   else:
-    store={'endpoint': {}, 'connection':{}, 'connection_type':{}}
+    store={}
 
-  if 'endpoint' in request.form:
-    ep=json.loads(request.form['endpoint'])
-    ep=json.dumps(ep)
-    store['endpoint'][ep]=request.form['uri']
-    if 'connection_type' in request.form:
-      store['connection_type'][ep]=request.form['connection_type']
-  if 'connection' in request.form:
-    conn=json.loads(request.form['connection'])
-    ep=json.dumps(conn['bind_endpoint'])
-    store['endpoint'][ep]=conn['uri']
-    store['connection'][ep]=conn
+  if 'connection_id' in request.form and 'uri' in request.form:
+    con=request.form['connection_id']
+    Connection=namedtuple('Connection',['uri','time'])
+    store[con]=Connection(request.form['uri'],datetime.now())
+    partitions[part]=store
+    return 'OK'
+  else:
+    abort(400)
 
+@app.route("/publishM",methods=['POST'])
+def publish_multi():
+  #  Store multiple connection ids and corresponding uris in a
+  #  dictionary associated with the appropriate partition.
+  js=json.loads(request.data)
+  #print (f"{js=}")
+  part=js['partition']
+  if part in partitions:
+    store=partitions[part]
+  else:
+    store={}
+  timestamp=datetime.now()
+  Connection=namedtuple('Connection',['uri','time'])
+  for con in js['connections']:
+    #print (f"{con=}")
+    id=con['connection_id']
+    store[id]=Connection(con['uri'],timestamp)
   partitions[part]=store
-  cmgr.update(partitions)
   return 'OK'
 
 @app.route("/retract-partition",methods=['POST'])
 def retract_partition():
   #print(f"retract_partition() request=[{request.form}]")
+  if 'partition' not in request.form:
+    abort(400)
   part=request.form['partition']
   if part in partitions:
     partitions.pop(part)
-    cmgr.delete(part)
     return 'OK'
   else:
     abort(404)
 
 @app.route("/retract",methods=['POST'])
 def retract():
-  print(f"retract() request=[{request.form}]")
-  part=request.form['partition']
+  js=json.loads(request.data)
+  good=True
+  part=js['partition']
   if part in partitions:
     store=partitions[part]
-    if 'endpoint' in request.form:
-      endpoint=json.loads(request.form['endpoint'])
-      endpoint=json.dumps(endpoint)
-      print(f"retracting endpoint <{endpoint}>")
-      if endpoint in store['endpoint']:
-        store['endpoint'].pop(endpoint)
-        partitions[part]=store
-        cmgr.update(partitions)
-        return 'OK'
+    for con in js['connections']:
+      #print (f"{con=}")
+      id=con['connection_id']
+      if id in store:
+        store.pop(id)
       else:
-        print(f"could not find endpoint <{endpoint}>")
-        abort(404)
-    elif 'connection' in request.form:
-      conn=json.loads(request.form['connection'])
-      ep=json.dumps(conn['bind_endpoint'])
-      if ep in store['connection']:
-        store['connection'].pop(ep)
-        store['endpoint'].pop(ep)
-        partitions[part]=store
-        cmgr.update(partitions)
-        return 'OK'
-      else:
-        abort(404)
-  else:
-    abort(404)
-
-@app.route("/getendpoint/<part>",methods=['POST','GET'])
-def get_endpoint(part):
-  if part in partitions:
-    store=partitions[part]
-
-    endpoint=json.loads(request.form['endpoint'])
-    matches=[]
-    ctype=""
-    for entry in store['endpoint']:
-      match=True
-      ep=json.loads(entry)
-      for item in endpoint:
-        #print(f"item=<{item}> entry=<{ep[item]}> search=<{endpoint[item]}>")
-        if ep[item]!=endpoint[item] and endpoint[item] != '*':
-          match=False
-      if match:
-        matches.append(store['endpoint'][entry])
-        if entry in store['connection']:
-          conn=store['connection'][entry]
-          ctype=conn['connection_type']
-        if entry in store['connection_type']:
-          ctype=store['connection_type'][entry]
-    if len(matches)>0:
-      result='{'+f'"uris":['
-      for n in range(len(matches)):
-        result=result+'"'+matches[n]+'"'
-        if n<len(matches)-1:
-          result=result+','
-      result=result+'],'+f'"connection_type":"{ctype}"'+'}'
-      #print(f"result = <{result}>")
-      dummy=json.loads(result)
-      print(f"json   = <{result}>")
-      return(result)
+        print(f"could not find connection_id <{id}>")
+        good=False
+    if len(store)>0:
+      partitions[part]=store
     else:
-      abort(404)
-  else:
-    #print(f"Partition {part} not found")
-    abort(404)
+      # We've deleted the last entry in this partition so delete the
+      # partition as well
+      partitions.pop(part)
 
-def lookup(part, map, key):
-  if part in partitions:
-    store=partitions[part]
-    #print(f"Looking for <{key}> in <{store[map]}>")
-    if key in store[map]:
-      val=store[map][key]
-      #print(f"lookup key={key} {map}[key]={val}")
-      return(val)
+    if good:
+      return 'OK'
     else:
       abort(404)
   else:
@@ -203,8 +116,25 @@ def lookup(part, map, key):
 
 @app.route("/getconnection/<part>",methods=['POST','GET'])
 def get_connection(part):
-  # Get the connection associated with an endpoint
-  ep=json.loads(request.form['endpoint'])
-  print(f"ep=<{ep}")
-  return lookup(part,'connection',json.dumps(ep))
+  # Find connection uris that correspond to the connection id pattern
+  # in the request. The pattern is treated as a regular expression.
+  if part in partitions:
+    store=partitions[part]
+    if 'connection_id' in request.form:
+      pat=request.form['connection_id']
+      #print(f"Searching for connections matching {pat}")
+      result=[]
+      regex=re.compile(pat)
+      now=datetime.now()
+      # Now try to find all matching entries for this connection
+      for id,con in store.items():
+        if regex.search(id) and now-con.time<entry_ttl:
+          #print (f"Found matching entry {id}")
+          result.append(con.uri)
+      return json.dumps(result)
+    else:
+      abort(400)
+  else:
+    print(f"Partition {part} not found")
+    abort(404)
 
