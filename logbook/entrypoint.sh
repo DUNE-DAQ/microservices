acf689c0498f0a49325aa4d78f3df187d34396de
John Freeman <jcfree@fnal.gov>
2023-09-12 17:38:47 +0000

    JCF: DUNE-DAQ/daq-deliverables#105: implement some changes to the Dockerfile suggested by Pat and Bonnie
    Specifically:
    -Reduce the number of layers in the standard microservices image
    -Move elisa_client_api installation out of the logbook's entrypoint script and into the Dockerfile
    -Post a note suggesting that in the future we not run as root inside the microservices container

--- logbook/entrypoint.sh
+++ logbook/entrypoint.sh
@@ -3,12 +3,5 @@
 mkdir -p ./logfiles
 cp ./cern-get-sso-cookie /usr/local/bin
 
-if [[ ! -d ./elisa_client_api ]]; then
-  git clone https://github.com/DUNE-DAQ/elisa_client_api.git
-fi
-
-pip3 install --upgrade setuptools
-pip3 install ./elisa_client_api 
-
 python3 ./logbook.py
 
