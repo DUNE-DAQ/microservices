99366ad6faa1fd4b1faeb64f56668af5d3998360
Pat Riehecky <riehecky@fnal.gov>
2023-10-09 15:44:58 -0500

    Update for new var

--- ers-dbwriter/entrypoint.sh
+++ ers-dbwriter/entrypoint.sh
@@ -3,6 +3,6 @@
 cd $(dirname $0)
 source ../entrypoint_functions.sh
 
-ensure_required_variables "ERS_DBWRITER_HOST ERS_DBWRITER_PORT ERS_DBWRITER_USER ERS_DBWRITER_PASS ERS_DBWRITER_NAME"
+ensure_required_variables "ERS_DBWRITER_HOST ERS_DBWRITER_PORT ERS_DBWRITER_USER ERS_DBWRITER_PASS ERS_DBWRITER_NAME ERS_DBWRITER_KAFKA_BOOTSTRAP_SERVER"
     
 python3 ./dbwriter.py
