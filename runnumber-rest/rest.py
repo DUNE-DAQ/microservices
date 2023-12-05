f7e2487b67f445245cbbc333e3d2eaacec070144
Pat Riehecky <riehecky@fnal.gov>
2023-12-05 12:38:31 -0600

    Fix duplicate declare

--- runnumber-rest/rest.py
+++ runnumber-rest/rest.py
@@ -16,4 +16,5 @@ DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 with app.app_context():
     db.create_all()
 
-app.run(host="0.0.0.0", port=5000, debug=DEBUG)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=DEBUG)
