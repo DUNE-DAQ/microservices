3771beaf1c8b87026bc5ce35492efce4eb3d3e73
Pat Riehecky <riehecky@fnal.gov>
2023-12-05 11:40:21 -0600

    Minor nits in entrypoint.sh

--- runregistry-rest/entrypoint.sh
+++ runregistry-rest/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-
 cd $(dirname $0)
 source ../entrypoint_functions.sh
 
 ensure_required_variables "DATABASE_URI"
 
-
-mkdir --mode=1777 /uploads
+if [[ ! -d /uploads ]]; then
+    mkdir --mode=1777 /uploads
+else
+    chmod 1777 /uploads
+fi
 
 exec gunicorn -b 0.0.0.0:5005 --workers=1 --worker-class=gevent --timeout 5000000000 --log-level=debug rest:app
