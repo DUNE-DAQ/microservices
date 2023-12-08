60c96f6b404919e9dc68e58f8c5a7bb47b35f5e7
titavare <tta20@ic.ac.uk>
2023-12-08 15:11:12 +0100

    added a row for the inital run so that all subsequent runs can platform off of this one

--- runnumber-rest/rest.py
+++ runnumber-rest/rest.py
@@ -9,11 +9,15 @@ DEBUG - set to TRUE for flask debugging
 import os
 
 from api import app, db
+from database import RunNumber
 
 # setenv DEBUG=True to enable debug mode
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 with app.app_context():
+    initial_run = RunNumber(rn='1000', flag='True', start_time='datetime.now', stop_time='')
+    db.session.add(initial_run)
+    db.session.commit()
     db.create_all()
 
 if __name__ == "__main__":
