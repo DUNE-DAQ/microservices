855a0dabdd85217dd0c6adedbe7a7aa1ecaaf823
titavare <tta20@ic.ac.uk>
2023-12-14 22:57:15 +0100

    asthetic changes

--- runnumber-rest/database.py
+++ runnumber-rest/database.py
@@ -8,9 +8,13 @@ __all__ = ["RunNumber"]
 class RunNumber(db.Model):
     rn = db.Column(
         "rn", db.Integer, primary_key=True, autoincrement=True, nullable=False
-    )
-    flag = db.Column("flag", db.Boolean, nullable=False, default=False)
+        )
+    flag = db.Column(
+        "flag", db.Boolean, nullable=False, default=False
+        )
     start_time = db.Column(
         "start_time", db.TIMESTAMP(6), nullable=False, default=datetime.now
-    )
-    stop_time = db.Column("stop_time", db.TIMESTAMP(6), nullable=True)
+        )
+    stop_time = db.Column(
+        "stop_time", db.TIMESTAMP(6), nullable=True
+        )
--- runregistry-rest/database.py
+++ runregistry-rest/database.py
@@ -9,19 +9,31 @@ __all__ = ["RunRegistryConfig", "RunRegistryMeta"]
 class RunRegistryConfig(db.Model):
     rn = db.Column(
         "rn", db.Integer, primary_key=True, autoincrement=True, nullable=False
-    )
-    configuration = db.Column("configuration", db.LargeBinary, nullable=False)
+        )
+    configuration = db.Column(
+        "configuration", db.LargeBinary, nullable=False
+        )
 
 
 class RunRegistryMeta(db.Model):
     rn = db.Column(
         "rn", db.Integer, db.ForeignKey("RunRegistryConfig.rn"), primary_key=True, autoincrement=True, nullable=False
-    )
+        )
     start_time = db.Column(
         "start_time", db.TIMESTAMP(6), nullable=False, default=datetime.now
-    )
-    stop_time = db.Column("stop_time", db.TIMESTAMP(6), nullable=True)
-    detector_id = db.Column("detector_id", db.String(40))
-    run_type = db.Column("run_type", db.String(40))
-    filename = db.Column("filename", db.String(100))
-    software_version = db.Column("software_version", db.String(40))
+        )
+    stop_time = db.Column(
+        "stop_time", db.TIMESTAMP(6), nullable=True
+        )
+    detector_id = db.Column(
+        "detector_id", db.String(40)
+        )
+    run_type = db.Column(
+        "run_type", db.String(40)
+        )
+    filename = db.Column(
+        "filename", db.String(100)
+        )
+    software_version = db.Column(
+        "software_version", db.String(40)
+        )
