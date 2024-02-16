bf72cbabb34b639ecc8f6e57f4cf4303df6269a7
Pat Riehecky <riehecky@fnal.gov>
2024-02-16 14:23:19 -0600

    Switch to open source oracle client
    fewer features, but easy to redistribute/build off site

--- runnumber-rest/entrypoint.sh
+++ runnumber-rest/entrypoint.sh
@@ -5,6 +5,4 @@ source ../entrypoint_functions.sh
 
 ensure_required_variables "DATABASE_URI" 
 
-export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib/:$LD_LIBRARY_PATH
-
 exec gunicorn -b 0.0.0.0:5000 --workers=1 --worker-class=gevent --timeout 5000000000 --log-level=debug rest:app
--- runregistry-rest/entrypoint.sh
+++ runregistry-rest/entrypoint.sh
@@ -11,6 +11,4 @@ else
     chmod 1777 uploads
 fi
 
-export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib/:$LD_LIBRARY_PATH
-
 exec gunicorn -b 0.0.0.0:5005 --workers=1 --worker-class=gevent --timeout 5000000000 --log-level=debug rest:app
