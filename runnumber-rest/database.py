27f5c24d4e8c1e44f88258c6efd9fe3f18771010
titavare <tta20@ic.ac.uk>
2024-02-26 12:03:47 +0100

    oracles doesn't like quotation marks so changed to inverted commas

--- runnumber-rest/database.py
+++ runnumber-rest/database.py
@@ -2,15 +2,15 @@ from datetime import datetime
 
 from api import db
 
-__all__ = ["RunNumber"]
+__all__ = ['RunNumber']
 
 
 class RunNumber(db.Model):
     rn = db.Column(
-        "rn", db.Integer, primary_key=True, autoincrement=True, nullable=False
+        'rn', db.Integer, primary_key=True, autoincrement=True, nullable=False
     )
-    flag = db.Column("flag", db.Boolean, nullable=False, default=False)
+    flag = db.Column('flag', db.Boolean, nullable=False, default=False)
     start_time = db.Column(
-        "start_time", db.TIMESTAMP(6), nullable=False, default=datetime.now
+        'start_time', db.TIMESTAMP(6), nullable=False, default=datetime.now
     )
-    stop_time = db.Column("stop_time", db.TIMESTAMP(6), nullable=True)
+    stop_time = db.Column('stop_time', db.TIMESTAMP(6), nullable=True)
