fddd23fd87a47f2a9c59eff9a074462c14bcde79
Pat Riehecky <riehecky@fnal.gov>
2024-02-29 13:19:23 -0600

    Run entrypoint functions through shfmt and shellcheck

--- entrypoint_functions.sh
+++ entrypoint_functions.sh
@@ -1,24 +1,15 @@
-
 function ensure_required_variables() {
+    local vars_as_string=$1
 
-    vars_as_string=$1
-    
-    IFS=' ' read -ra vars <<< "$vars_as_string"
+    IFS=' ' read -ra vars <<<"$vars_as_string"
 
-    missing_variable=false
-    
     for var in "${vars[@]}"; do
-
-	if [[ -v $var ]]; then
-	    echo "$var is defined as \"${!var}\"."
-	else
-	    echo "$var needs to be defined as an environment variable."
-	    missing_variable=true
-	fi
+        if [[ -v $var ]]; then
+            echo "$var is defined as \"${!var}\"."
+        else
+            echo "$var needs to be defined as an environment variable."
+            echo "One or more required environment variables is undefined; exiting..." >&2
+            exit 3
+        fi
     done
-
-    if $missing_variable ; then
-	echo "One or more required environment variables is undefined; exiting..." >&2
-	exit 3
-    fi
 }
