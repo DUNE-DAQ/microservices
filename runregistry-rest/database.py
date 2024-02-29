b611a3b44f8fc51337afc01332b039a4e0381ab3
Pat Riehecky <riehecky@fnal.gov>
2024-02-29 12:09:07 -0600

    Since we're using the default names, just let the names default

--- runregistry-rest/database.py
+++ runregistry-rest/database.py
@@ -7,23 +7,15 @@ __all__ = ["RunNumber", "RunRegistryConfig", "RunRegistryMeta"]
 
 class RunNumber(db.Model):
     run_number = db.Column(
-        "run_number",
-        db.Integer,
-        primary_key=True,
-        autoincrement=True,
-        nullable=False,
-        index=True,
+        db.Integer, primary_key=True, autoincrement=True, nullable=False, index=True
     )
-    flag = db.Column("flag", db.Boolean, nullable=False, default=False)
-    start_time = db.Column(
-        "start_time", db.TIMESTAMP(6), nullable=False, default=datetime.now
-    )
-    stop_time = db.Column("stop_time", db.TIMESTAMP(6), nullable=True)
+    flag = db.Column(db.Boolean, nullable=False, default=False)
+    start_time = db.Column(db.TIMESTAMP(6), nullable=False, default=datetime.now)
+    stop_time = db.Column(db.TIMESTAMP(6), nullable=True)
 
 
 class RunRegistryConfig(db.Model):
     run_number = db.Column(
-        "run_number",
         db.Integer,
         db.ForeignKey(RunNumber.run_number),
         primary_key=True,
@@ -31,12 +23,11 @@ class RunRegistryConfig(db.Model):
         nullable=False,
         index=True,
     )
-    configuration = db.Column("configuration", db.LargeBinary, nullable=False)
+    configuration = db.Column(db.LargeBinary, nullable=False)
 
 
 class RunRegistryMeta(db.Model):
     run_number = db.Column(
-        "run_number",
         db.Integer,
         db.ForeignKey(RunNumber.run_number),
         primary_key=True,
@@ -44,7 +35,7 @@ class RunRegistryMeta(db.Model):
         nullable=False,
         index=True,
     )
-    detector_id = db.Column("detector_id", db.String(40))
-    run_type = db.Column("run_type", db.String(40))
-    filename = db.Column("filename", db.String(100))
-    software_version = db.Column("software_version", db.String(40))
+    detector_id = db.Column(db.String(40))
+    run_type = db.Column(db.String(40))
+    filename = db.Column(db.String(100))
+    software_version = db.Column(db.String(40))
