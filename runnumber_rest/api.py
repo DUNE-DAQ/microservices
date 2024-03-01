943fff5e5ecae9df0e3c24314dc8fcf477eb7efc
titavare <tta20@ic.ac.uk>
2024-03-01 11:11:11 +0100

    changed runnumber api to macth runregistry api

--- runnumber_rest/api.py
+++ runnumber_rest/api.py
@@ -10,10 +10,14 @@ __all__ = ["app", "api", "db"]
 
 app = flask.Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    "DATABASE_URI", "sqlite:////tmp/test.sqlite"
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=os.environ.get(
+        "DATABASE_URI", "sqlite:////tmp/test.sqlite"
+    ),
+    DEPLOYMENT_ENV=os.environ.get("DEPLOYMENT_ENV", "DEV"),
+    RUN_START=int(os.getenv("RUN_START", "1000")),
+    SQLALCHEMY_ECHO=False,
 )
-app.config["DEPLOYMENT_ENV"] = os.environ.get("DEPLOYMENT_ENV", "DEV")
 
 uri = app.config["SQLALCHEMY_DATABASE_URI"]
 db = SQLAlchemy(app)
@@ -21,16 +25,15 @@ api = Api(app)
 
 from urllib.parse import urlparse
 
-from authentication import auth
-from database import RunNumber
-
-parsed_uri = urlparse(uri)
+PARSED_URI = urlparse(uri)
 db_type = parsed_uri.scheme
 print(db_type)
 
+from runregistry_rest.authentication import auth
+from runregistry_rest.database import RunNumber
 
 # $ curl -u fooUsr:barPass -X GET np04-srv-021:30016//runnumber/get
-@api.resource("/runnumber/get")
+@api.resource("/runnumber/get") ## Keep this for now 
 class getRunNumber(Resource):
     """
     returns the run number of the previous run
@@ -135,6 +138,21 @@ __emails__ = [
 
 @app.route("/")
 def index():
+    def generate_endpoint_html(
+        method, link, url, description, example_curl, notes=None
+    ):
+        result = f"""
+        <div style="border: 1px solid black">
+            <h3>{method} <a href="{link}">{url}</a></h3>
+            <p>{description}<br /><br />
+            <strong>Example:</strong><br />
+            <code style="font-size: 1.5em;">{example_curl}</code></p>
+        """
+        if notes:
+            result += f"<h4>Notes:</h4>\n{notes}"
+        result += "</div>\n<hr />"
+        return result
+
     root_text = f"""
     <!DOCTYPE html>
     <html>
@@ -149,32 +167,16 @@ def index():
         <li>emails: {__emails__}</li>
     </ul>
 
-    <h2>Endpoints</h2>
-    <div style="border: 1px solid black">
-    <h3>GET /runnumber/get</h3>
-    <p>Gets the current or last run number.</p>
-    <p>Example:</p>
-    <p style="font-family:courier;">$ curl -u user:password -X GET http://host:port/runnumber/get</p>
-    </div>
-    <p></p>
-
-    <div style="border: 1px solid black">
-    <h3>GET /runnumber/getnew</h3>
-    <p>Get a new unique run number.</p>
-    <p>Example:</p>
-    <p style="font-family:courier;">$ curl -u user:password -X GET http://host:port/runnumber/getnew</p>
-    </div>
-    <p></p>
-
-    <div style="border: 1px solid black">
-    <h3>GET /runnumber/updatestop/<run_num></h3>
-    <p>Update the stop time of the specified run number (replace <run_num> with the run number you want).</p>
-    <p>Example:</p>
-    <p style="font-family:courier;">$ curl -u user:password -X GET http://host:port//runnumber/updatestop/2</p>
-    </div>
-    <p></p>
-
+    <h2>Endpoints:</h2>
+    {generate_endpoint_html("GET", "/runregistry/get", "/runregistry/get", "Gets the current or last run number.", "$ curl -u user:password -X GET http://host:port/runregistry/get")}
+    {generate_endpoint_html("GET", "/runregistry/getnew", "/runregistry/getnew", "Get a new unique run number.", "$ curl -u user:password -X GET http://host:port/runregistry/getnew")}
+    {generate_endpoint_html("GET", "/runregistry/updatestop/", "/runregistry/updatestop/&lt;run_num&gt;", "Update the stop time of the specified run number (replace <run_num> with the run number you want).", "$ curl -u user:password -X GET http://host:port/runregistry/updatestop/2")}
+    {generate_endpoint_html("GET", "/runregistry/getRunMeta/", "/runregistry/getRunMeta/&lt;run_num&gt;", "Gets the run metadata for the specified run number (replace <run_num> with the run number you want).", "$ curl -u user:password -X GET http://host:port/runregistry/getRunMeta/2")}
+    {generate_endpoint_html("GET", "/runregistry/getRunMetaLast/", "/runregistry/getRunMetaLast/&lt;how_many_runs&gt;", "Get the run metadata for the last runs (replace <how_many_runs> by the number of runs you want to go in the past).", "$ curl -u user:password -X GET http://host:port/runregistry/getRunMetaLast/100")}
+    {generate_endpoint_html("GET", "/runregistry/getRunBlob/", "/runregistry/getRunBlob/&lt;run_num&gt;", "Get the run configuration blob (tar.gz of some folders structure containing json) for the specified run number (replace <run_num> by the run number you want).", "$ curl -u user:password -X GET -O -J http://host:port/runregistry/getRunBlob/2")}
+    {generate_endpoint_html("POST", "/runregistry/insertRun/", "/runregistry/insertRun/", "Insert a new run in the database. The post request should have the fields:", "$ curl -u user:password -F 'file=@sspconf.tar.gz' -F 'run_num=4' -F 'det_id=foo' -F 'run_type=bar' -F 'software_version=dunedaq-vX.Y.Z' -X POST http://host:port/runregistry/insertRun/", "<p>To insert a new run into the database. The post request should have the fields:</p>\n<ul>\n<li>'file': a file containing the configuration to save</li>\n<li>'run_num': the run number</li>\n<li>'det_id': the id of the detector</li><li>'run_type': the type of run (either PROD of TEST)</li>\n<li>'software_version': the version of dunedaq.</li>\n</ul>")}
     </body>
     </html>
     """
+
     return root_text
