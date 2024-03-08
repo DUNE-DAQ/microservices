4fd713e1436942458df90e390829fd38c4e66dd1
titavare <tta20@ic.ac.uk>
2024-03-08 17:00:32 +0100

    removed foreign key because otherwise you can't add runs

--- runregistry-rest/database.py
+++ runregistry-rest/database.py
@@ -30,7 +30,7 @@ class RunRegistryMeta(db.Model):
 
 class RunRegistryConfig(db.Model):
     run_number = db.Column(
-        'run_number', db.Integer, db.ForeignKey(RunRegistryMeta.run_number),  primary_key=True, autoincrement=True, nullable=False
+        'run_number', db.Integer,  primary_key=True, autoincrement=True, nullable=False
     )
     configuration = db.Column(
         'configuration', db.LargeBinary, nullable=False
