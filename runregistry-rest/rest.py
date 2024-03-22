9f7d8a746832bcc2b36b30e7c3014d8f56599695
titavare <tta20@ic.ac.uk>
2024-03-22 10:26:24 +0100

    trying to create Meta first and then Configs

--- runregistry-rest/rest.py
+++ runregistry-rest/rest.py
@@ -15,7 +15,9 @@ from api import app, db
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
 with app.app_context():
-    db.create_all()
+    db.session.commit()
+    db.RunRegistryMeta.__table__.create(db.engine)
+    db.RunRegistryConfigs.__table__.create(db.engine)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5005, debug=DEBUG)
