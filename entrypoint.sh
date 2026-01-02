1f92666f0552d93603167414dedf33dcbf267989
Pat Riehecky <riehecky@fnal.gov>
2026-01-02 10:47:38 -0600

    Explicit use of paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,15 @@ ensure_required_variables "MICROSERVICE"
 microservice_dir=$(pwd)/$MICROSERVICE
 
 if [[ ! -e ${microservice_dir}/entrypoint.sh ]]; then
-    echo "This script sees the MICROSERVICE environment variable set to \"$MICROSERVICE\" but is unable to find the corresponding entrypoint script \"${microservice_dir}/entrypoint.sh\"" >&2
+    echo "This script sees the MICROSERVICE environment variable set to \"${MICROSERVICE}\" but is unable to find the corresponding entrypoint script \"${microservice_dir}/entrypoint.sh\"" >&2
     exit 2
 fi
 
 cd $microservice_dir
 
-./entrypoint.sh
+$(pwd)/entrypoint.sh
 
 retval=$?
-echo "Return value of call to ${microservice_dir}/entrypoint.sh is $retval"
+echo "Return value of call to ${microservice_dir}/entrypoint.sh is:${retval}"
 
 exit $retval
