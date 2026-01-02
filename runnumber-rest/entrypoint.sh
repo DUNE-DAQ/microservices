9b9d26236977a8c90c2c6bc45274e854c400b0aa
Pat Riehecky <riehecky@fnal.gov>
2026-01-02 10:06:47 -0600

    gevent makes no sense with 1 worker, use default workers

--- runnumber-rest/entrypoint.sh
+++ runnumber-rest/entrypoint.sh
@@ -5,4 +5,4 @@ source ../entrypoint_functions.sh
 
 ensure_required_variables "DATABASE_URI"
 
-exec gunicorn -b 0.0.0.0:5000 --workers=1 --worker-class=gevent --timeout 5000000000 --log-level=debug rest:app
+exec gunicorn -b 0.0.0.0:5000 --timeout 5000000000 --log-level=debug rest:app
