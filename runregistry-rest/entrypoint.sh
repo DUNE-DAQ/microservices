fee3fce21e28f2f24deee43df32c6aeaa78c1655
Pat Riehecky <riehecky@fnal.gov>
2026-01-03 00:15:10 -0600

    Make python responsible for mkdir via env var for non-root runtime

--- runregistry-rest/entrypoint.sh
+++ runregistry-rest/entrypoint.sh
@@ -3,12 +3,6 @@
 cd $(dirname $0)
 source ../entrypoint_functions.sh
 
-ensure_required_variables "DATABASE_URI"
-
-if [[ ! -d uploads ]]; then
-    mkdir --mode=1777 uploads
-else
-    chmod 1777 uploads
-fi
+ensure_required_variables "DATABASE_URI APP_DATA"
 
 exec gunicorn -b 0.0.0.0:5005 --workers=1 --worker-class=gevent --timeout 5000000000 --log-level=debug rest:app
