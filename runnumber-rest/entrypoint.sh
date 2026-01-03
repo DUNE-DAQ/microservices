a31360f241940e9bde34eac1a1cfb0962c42f17f
Pat Riehecky <riehecky@fnal.gov>
2026-01-03 11:41:11 -0600

    Move to AL10

--- runnumber-rest/entrypoint.sh
+++ runnumber-rest/entrypoint.sh
@@ -10,4 +10,4 @@ source ../entrypoint_functions.sh
 
 ensure_required_variables "DATABASE_URI"
 
-exec gunicorn -b 0.0.0.0:5000 --workers=1 --worker-class=gevent --timeout 5000000000 --log-level=debug rest:app
+exec gunicorn -b 0.0.0.0:5000 --timeout 9000 --log-level=debug rest:app
