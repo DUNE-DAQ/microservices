15865493be21804ab63cf70c27d5620fffa421f0
Pat Riehecky <riehecky@fnal.gov>
2026-01-05 08:35:08 -0600

    Drop deprecated handler

--- runregistry-rest/api.py
+++ runregistry-rest/api.py
@@ -56,16 +56,6 @@ PARSED_URI = urlparse(app.config["SQLALCHEMY_DATABASE_URI"])
 DB_TYPE = PARSED_URI.scheme
 
 
-@app.before_first_request
-def register_event_handlers():
-    @event.listens_for(db.engine, "handle_error")
-    def handle_exception(context):
-        if not context.is_disconnect and re.match(
-            r"^(?:DPI-1001|DPI-4011)", str(context.original_exception)
-        ):
-            context.is_disconnect = True
-
-
 def cache_key():
     args = flask.request.args
     key = (
