89fe2834b23e6c239cf8f528d1aebe5f6d719f09
Pat Riehecky <riehecky@fnal.gov>
2026-01-05 13:40:16 -0600

    Fix potential missing exit codes

--- ers-protobuf-dbwriter/entrypoint.sh
+++ ers-protobuf-dbwriter/entrypoint.sh
@@ -9,7 +9,7 @@ source ../entrypoint_functions.sh
 
 ensure_required_variables "ERS_DBWRITER_KAFKA_BOOTSTRAP_SERVER ERS_DBWRITER_KAFKA_TIMEOUT_MS ERS_DBWRITER_KAFKA_GROUP ERS_DBWRITER_DB_URI ERS_DBWRITER_DB_TABLENAME"
 
-python3 ./dbwriter.py --subscriber-bootstrap "${ERS_DBWRITER_KAFKA_BOOTSTRAP_SERVER}" \
+exec python3 ./dbwriter.py --subscriber-bootstrap "${ERS_DBWRITER_KAFKA_BOOTSTRAP_SERVER}" \
                       --subscriber-group "${ERS_DBWRITER_KAFKA_GROUP}" \
                       --subscriber-timeout "${ERS_DBWRITER_KAFKA_TIMEOUT_MS}" \
                       --db-uri "${ERS_DBWRITER_DB_URI}" \
