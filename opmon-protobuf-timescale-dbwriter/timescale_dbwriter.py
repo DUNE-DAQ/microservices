16aeddd14441f026bdf61a771f1f66ec2da7e684
Henry Wallace <henry.wallace@rhul.ac.uk>
2026-08-10 14:46:20 +0100

    add GIN indices

--- opmon-protobuf-timescale-dbwriter/timescale_dbwriter.py
+++ opmon-protobuf-timescale-dbwriter/timescale_dbwriter.py
@@ -6,71 +6,69 @@
 import json
 import logging
 import queue
+import socket as _socket
 import threading
 from functools import partial
+from threading import Thread
 from urllib.parse import urlparse
 
 import click
 import kafkaopmon.OpMonSubscriber as opmon_sub
 import opmonlib.opmon_entry_pb2 as opmon_schema
 
-from sqlalchemy import (Engine,
-                        create_engine,
-                        text,
-                        MetaData,
-                        Table,
-                        Column,
-                        DateTime,
-                        JSON,
-                        Text)
-
+from sqlalchemy import (
+    Column,
+    DateTime,
+    Engine,
+    Index,
+    JSON,
+    MetaData,
+    Table,
+    Text,
+    create_engine,
+    text,
+)
 from sqlalchemy.exc import OperationalError, SQLAlchemyError
-
-from sqlalchemy_utils import database_exists, create_database
-
-import socket as _socket
-from threading import Thread
+from sqlalchemy_utils import create_database, database_exists
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
-TABLE_SCHEMA = {"time": "TIMESTAMPTZ",
-                "measurement": "TEXT",
-                "tags": "JSONB",
-                "fields": "JSONB"}
 OPMON_TABLE_PREFIX = "opmon_entries_"
-
 
 logger = logging.getLogger(__name__)
 metadata = MetaData()
 timescale_engine = None
 _health_server_socket = None
 
-_tables_cache = {}
 _tables_lock = threading.Lock()
 
-
 # ------- SCHEMA --------- #
-def _table_schema(measurement: str):
-    # Create table + schema definition
-    table_name = f"{OPMON_TABLE_PREFIX}{measurement}"
+def _table_name_from_measurement(measurement: str):
+    return f"{OPMON_TABLE_PREFIX}{measurement}"
+
+def _table_schema(table_name: str):
     return Table(
         table_name,
         metadata,
         Column("time", DateTime(timezone=True)),
         Column("measurement", Text),
         Column("tags", JSON),
-        Column("fields", JSON),
+        Column("fields", JSON),    
+        Index(f"ix_{table_name}_tags_gin", "tags", postgresql_using="gin"),
+        Index(f"ix_{table_name}_fields_gin", "fields", postgresql_using="gin"),
     )
+
 
 # ------- Opmon Processing -------- #
 class Entry:
-    def __init__(self, json: dict, ms: int):
-        self.json = json
+    def __init__(self, json_data: dict, ms: int):
+        self.json = json_data
         self.ms = ms
+
 
 def process_entry(entry: opmon_schema.OpMonEntry, q: queue.Queue):
     d = to_dict(entry)
-    e = Entry(json=d, ms=entry.time.ToMilliseconds())
+    e = Entry(json_data=d, ms=entry.time.ToMilliseconds())
     q.put(e)
 
 
@@ -78,37 +76,35 @@ def to_dict(entry: opmon_schema.OpMonEntry) -> dict:
     ret = dict(measurement=entry.measurement)
     ret["fields"] = unpack_payload(entry)
     ret["tags"] = create_tags(entry)
-    ret["time"] = entry.time.ToJsonString()
+    # Convert Protobuf Timestamp directly to Python datetime
+    ret["time"] = entry.time.ToDatetime()
     return ret
 
 
 def unpack_payload(entry: opmon_schema.OpMonEntry) -> dict:
     data = entry.data
     ret = dict()
-    for key in data:
-        value = data[key]
-        casted_value = getattr(value, value.WhichOneof("kind"))
-        ret[key] = casted_value
-
+    for key, value in data.items():
+        kind = value.WhichOneof("kind")
+        if kind is not None:
+            ret[key] = getattr(value, kind)
     return ret
+
 
 def create_tags(entry: opmon_schema.OpMonEntry) -> dict:
     opmon_id = entry.origin
-    # session and application
     tags = dict(session=opmon_id.session, application=opmon_id.application)
 
-    # element and subelements
     struct = opmon_id.substructure
     for i, s in enumerate(struct):
         name = "sub" * i + "element"
         tags[name] = s
 
-    # custom origin
-    tags |= entry.custom_origin
-
+    tags.update(entry.custom_origin)
     return tags
 
-# --- Health Client Tools --- #
+
+# --- Health Client Tools --- #
 def _handle_health_client(conn):
     try:
         data = b""
@@ -159,6 +155,7 @@ def _health_accept_loop(sock):
         except OSError:
             break
 
+
 def start_health_server(port: int):
     global _health_server_socket
     _health_server_socket = _socket.socket(_socket.AF_INET, _socket.SOCK_STREAM)
@@ -174,71 +171,57 @@ def uri_to_db_name(uri: str):
     parsed_uri = urlparse(uri)
     return parsed_uri.path.lstrip("/")
 
-def _connect_timescale(timescaledb_uri: str, timescaledb_create: bool)->Engine:
-    db_name = uri_to_db_name(timescaledb_uri)
-    if not db_name:
-        raise ValueError("No database name in URI")
 
-    engine = create_engine(timescaledb_uri)
 
-    if database_exists(engine.url):
-        return engine
 
-    if not timescaledb_create:
-        raise ValueError(f"Cannot find {db_name} DB")
-
-    create_database(engine.url)
-
-    return engine
-
-def consume(q: queue.Queue, timeout_ms, timescale_db: Engine | None = None):
+def consume(q: queue.Queue, timeout_ms: int, timescale_db: Engine | None = None):
     logger.info("Starting consumer thread")
     batch = {}
-    batch_ms = 0
+    batch_start_ms = None
+
     while True:
         try:
-            entry = q.get(timeout=1)  ## timeout here is in seconds
+            entry = q.get(timeout=1.0)
+            now_ms = entry.ms
 
-            if entry.ms - batch_ms < timeout_ms:
-                # because of the if, batch_ms is not zero.
-                measure = entry.json['measurement']
-                batch_measure = batch.get(measure, [])
-                batch_measure.append(entry.json)
-                batch[measure] = batch_measure
-                batch_ms = min(batch_ms, entry.ms)
+            if batch_start_ms is None:
+                batch_start_ms = now_ms
 
-            if entry.ms - batch_ms >= timeout_ms:
-                # note that if we are facing with a late arrival, i.e. entry.ms was smaller than batch_ms, the difference is 0, so this if is skipped
-                # i.e. there is not double insertion
+            measure = entry.json["measurement"]
+            batch.setdefault(measure, []).append(entry.json)
+
+            if now_ms - batch_start_ms >= timeout_ms:
                 send_batch(batch, timescale_db)
-                batch[entry.json['measurement']] = entry.json
-                batch_ms = entry.ms
+                batch = {}
+                batch_start_ms = None
 
         except queue.Empty:
-            logger.debug("Queue is empty")
-            send_batch(batch, timescale_db)
-            batch = {}
-            batch_ms = 0
+            if batch:
+                send_batch(batch, timescale_db)
+                batch = {}
+                batch_start_ms = None
+
 
 def _find_or_create_table(measurement: str, engine: Engine):
-    # Looks up table in cache, if not found generates a new table
-    t = _tables_cache.get(measurement)
-    if not t:
-        t = _table_schema(measurement)
-        t.create(engine, checkfirst=True)
-        _tables_cache[measurement] = t
+    # Finds table in the metadata OR create a new one
+    table_name = _table_name_from_measurement(measurement)
+    # Prevent re-defining an existing table in metadata
+    if table_name in metadata.tables:
+        return metadata.tables[table_name]
+
+    t = _table_schema(table_name)
+    t.create(engine, checkfirst=True)
     return t
 
 def _generate_batch_tables(measurements: list[str], engine: Engine):
-    # Create tables for batch if they don't already exist
     with _tables_lock:
         return [_find_or_create_table(m, engine) for m in measurements]
 
-def send_batch(batch: dict[str, dict], engine: Engine | None = None):
-    
+def send_batch(batch: dict[str, list[dict]], engine: Engine | None = None):
     if len(batch) > 0:
-        logger.info("Sending %s points", len(batch))
-        
+        total_points = sum(len(v) for v in batch.values())
+        logger.info("Sending %s points across %s measurements", total_points, len(batch))
+
         if engine is not None:
             tables = _generate_batch_tables(list(batch.keys()), engine)
             try:
@@ -251,10 +234,64 @@ def send_batch(batch: dict[str, dict], engine: Engine | None = None):
                 logger.exception("Something went wrong: batch not sent")
         else:
             print(batch)
+        
 
 # --------- CLI --------- #
+@click.command(context_settings=CONTEXT_SETTINGS)
+# subscriber options
+@click.option(
+    "--subscriber-bootstrap",
+    type=click.STRING,
+    default="monkafka.cern.ch:30092",
+    help="boostrap server and port of the OpMonSubscriber",
+)
+@click.option(
+    "--subscriber-group",
+    type=click.STRING,
+    default=None,
+    help="group ID of the OpMonSubscriber",
+)
+@click.option(
+    "--subscriber-timeout",
+    type=click.INT,
+    default=500,
+    help="timeout in ms used in the OpMonSubscriber",
+)
+@click.option(
+    "--subscriber-topic",
+    type=click.STRING,
+    multiple=True,
+    default=["opmon_stream"],
+    help='The system will add the "monitoring." prefix',
+)
+# timescale options
+@click.option(
+    "--timescaledb_uri",
+    type=click.STRING,
+    default="postgres://localhost:8086/test_timescaledb",
+    help="URI of the timescaleDB server (e.g., postgres]://user:pass@host:port/dbname)",
+)
+@click.option(
+    "--timescaledb_create",
+    type=click.BOOL,
+    default=True,
+    help="Creates the timescaledb if it does not exists",
+)
+@click.option(
+    "--timescaledb_timeout",
+    type=click.INT,
+    default=500,
+    help="Size in ms of the batches sent to timescale",
+)
+@click.option("--debug", type=click.BOOL, default=True, help="Set debug print levels")
+@click.option(
+    "--health-port",
+    type=click.INT,
+    default=None,
+    help="Port for HTTP health e ndpoint (if not set, no health endpoint)",
+)
 def cli(
-     subscriber_bootstrap,
+    subscriber_bootstrap,
     subscriber_group,
     subscriber_timeout,
     subscriber_topic,
@@ -279,13 +316,13 @@ def cli(
         group_id=subscriber_group,
         timeout_ms=subscriber_timeout,
     )
-    
+
     q = queue.Queue()
-    
+
     callback_function = partial(process_entry, q=q)
     if health_port is not None:
         start_health_server(health_port)
-    
+
     sub.add_callback(name="to_timescale_db", function=callback_function)
 
     thread = threading.Thread(
@@ -294,3 +331,7 @@ def cli(
     thread.start()
 
     sub.start()
+
+
+if __name__ == "__main__":
+    cli()
