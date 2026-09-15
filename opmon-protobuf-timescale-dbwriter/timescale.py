caed25e5c18f95cf6218b1ab2757e590d7efed48
henry-israel <henryisrael08@gmail.com>
2026-09-15 11:42:24 +0100

    lowercase table name

--- opmon-protobuf-timescale-dbwriter/timescale.py
+++ opmon-protobuf-timescale-dbwriter/timescale.py
@@ -101,7 +101,7 @@ class SchemaManager:
             Measurement names must be validated by caller (OpMonTransformer).
         """
         # safe_measurement = measurement #_TABLE_SAFE_RE.sub("_", measurement).lower()
-        table_name = measurement #f"{OPMON_TABLE_PREFIX}{safe_measurement}"
+        table_name = measurement.lower() #f"{OPMON_TABLE_PREFIX}{safe_measurement}"
 
         with self._tables_lock:
             if table_name in self.metadata.tables:
