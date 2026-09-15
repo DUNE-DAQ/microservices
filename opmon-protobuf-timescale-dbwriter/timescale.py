d7fbfeba85edb8fdb973fd8e2acf3a05902728f2
henry-israel <henryisrael08@gmail.com>
2026-09-15 11:38:28 +0100

    Table name is now measurement

--- opmon-protobuf-timescale-dbwriter/timescale.py
+++ opmon-protobuf-timescale-dbwriter/timescale.py
@@ -100,8 +100,8 @@ class SchemaManager:
         Note:
             Measurement names must be validated by caller (OpMonTransformer).
         """
-        safe_measurement = _TABLE_SAFE_RE.sub("_", measurement).lower()
-        table_name = f"{OPMON_TABLE_PREFIX}{safe_measurement}"
+        # safe_measurement = measurement #_TABLE_SAFE_RE.sub("_", measurement).lower()
+        table_name = measurement #f"{OPMON_TABLE_PREFIX}{safe_measurement}"
 
         with self._tables_lock:
             if table_name in self.metadata.tables:
